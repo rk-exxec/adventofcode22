e2ec4975fed2c2e97f46297da4e85c37bfbe139a
Raphael <40037381+rk-exxec@users.noreply.github.com>
2022-12-05 14:05:38 +0100

    day4

---
+++ day04b.py
@@ -0,0 +1,16 @@
+with open("day04_input.txt") as file:
+    lines = file.readlines()
+
+total_overlaps = 0
+for line in lines:
+    p1,p2 = line.strip().split(",")
+    p1_low,p1_hi = p1.split("-")
+    p2_low,p2_hi = p2.split("-")
+
+    range1 = set(range(int(p1_low),int(p1_hi)+1))
+    range2 = set(range(int(p2_low),int(p2_hi)+1))
+
+    if not range1.isdisjoint(range2): total_overlaps += 1
+    
+
+print(total_overlaps)
